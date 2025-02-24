43269b7e882589506a7b5d1eabe3ca34c135c970
BrunoWilkinson <bruno.wilkinson@gmail.com>
2025-02-24 01:20:39 +0000

    Fix duplicated game.cpp

--- engine/Game.cpp
+++ engine/Game.cpp
@@ -16,6 +16,42 @@ Game::Game(const std::string& path, const std::string& window_name);
 
 void Game::init(const std::string& path, const std::string& window_name)
 {
+    ConfigManager configManager("config.txt");
+    sf::RenderWindow window(sf::VideoMode({configManager.getWindowWidth(), configManager.getWindowHeight()}), "Assignment 1");
+    window.setFramerateLimit(60);
+
+    if (!ImGui::SFML::Init(window))
+    {
+        return -1;
+    }
+    sf::Clock deltaClock;
+
+
+    while (window.isOpen())
+    {
+	sf::Event event;
+        while (window.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        window.clear();
+        for (Shape* shape : configManager.getShapes())
+        {
+            shape->draw(window, deltaClock, configManager.getFonts()[0]);
+        }
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
 }
 
 void Game::run()
