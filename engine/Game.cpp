3ae45113e04252399b0875f5d0f0ba89cce94eee
BrunoWilkinson <bruno.wilkinson@gmail.com>
2025-02-25 23:50:59 +0000

    Fix compiler errors

--- engine/Game.cpp
+++ engine/Game.cpp
@@ -1,59 +1,59 @@
 #include "imgui.h" // necessary for ImGui::*, imgui-SFML.h doesn't include imgui.h
 #include "imgui-SFML.h" // for ImGui::SFML::* functions and SFML-specific overloads
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Rect.hpp>
-#include <SFML/Graphics/Text.hpp>
-#include <SFML/Graphics/Font.hpp>
 #include <fstream>
 #include <iostream>
 #include <cmath>
 #include "Game.h"
 
-Game::Game(const std::string& path, const std::string& window_name);
+Game::Game(const std::string& path,
+           const std::string& window_name,
+           const unsigned int window_width,
+           const unsigned int window_height,
+           const unsigned int framerate_limit)
 {
-	init(path, window_name);
-}
+    m_window.create(
+        sf::VideoMode({window_width, window_height}),
+        window_name);
+    m_window.setFramerateLimit(framerate_limit);
 
-void Game::init(const std::string& path, const std::string& window_name)
-{
-    ConfigManager configManager("config.txt");
-    sf::RenderWindow window(sf::VideoMode({configManager.getWindowWidth(), configManager.getWindowHeight()}), "Assignment 1");
-    window.setFramerateLimit(60);
-
-    if (!ImGui::SFML::Init(window))
-    {
-        return -1;
-    }
-    sf::Clock deltaClock;
-
-
-    while (window.isOpen())
-    {
-	sf::Event event;
-        while (window.pollEvent(event))
-        {
-            ImGui::SFML::ProcessEvent(window, event);
-
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        window.clear();
-        for (Shape* shape : configManager.getShapes())
-        {
-            shape->draw(window, deltaClock, configManager.getFonts()[0]);
-        }
-        ImGui::SFML::Render(window);
-        window.display();
-    }
-
-    ImGui::SFML::Shutdown();
+    init();
 }
 
 void Game::run()
 {
 }
+
+void Game::init()
+{
+    if (!ImGui::SFML::Init(m_window))
+    {
+        // TODO: add logging error
+        return;
+    }
+
+    while (m_window.isOpen())
+    {
+	sf::Event event;
+        while (m_window.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(m_window, event);
+
+            if (event.type == sf::Event::Closed)
+            {
+                m_window.close();
+            }
+        }
+
+        ImGui::SFML::Update(m_window, m_delta_clock.restart());
+
+        m_window.clear();
+
+        // run ecs systems
+        run();
+
+        m_window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+}
+
--- engine/Game.h
+++ engine/Game.h
@@ -1,16 +1,24 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <SFML/Graphics.hpp>
 #include <string>
 
 class Game
 {
-	void init(const std::string& path, const std::string& window_name);
+    sf::Clock m_delta_clock;
+	sf::RenderWindow m_window;
+
+	virtual void run();
+	void init();
 
 public:
-	Game(const std::string& path, const std::string& window_name);
-	void run();
-
+	Game(const std::string& path,
+	  const std::string& window_name,
+	  const unsigned int window_width,
+	  const unsigned int window_height,
+	  const unsigned int frame_linit);
 };
+
 
 #endif
